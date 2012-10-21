076278c5d48050b05cf669ad18426fb0149ebfe8
kosystem <k.o.system+dev@gmail.com>
2012-10-21 19:17:16 +0900

    Initial Commit

---
+++ Matrix/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Matrix
+//
+//  Created by 小沼 和文 on 12/10/21.
+//  Copyright (c) 2012年 Kazufumi Onuma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
